a49aae6e87486dfb342c979398f887749fa10aba
doanvanquoc <vanquoc.dev@gmail.com>
2024-01-15 15:46:09 +0700

    edit get company api

--- services/company.js
+++ services/company.js
@@ -2,16 +2,24 @@ const db = require('../models')
 
 const getAllCompany = () => new Promise(async (resolve, reject) => {
     try {
-        const companies = await db.Company.findAll()
+        const companies = await db.Company.findAll({
+            include: [
+                {
+                    model: db.Product,
+                    required: true,
+                    attributes: []
+                }
+            ]
+        });
+
         if (companies) {
-            resolve({message: 'OK', data: companies})
-        }
-        else {
-            resolve({message: 'Không có công ty nào'})
+            resolve({ message: 'OK', data: companies });
+        } else {
+            resolve({ message: 'Không có công ty nào' });
         }
     } catch (error) {
-        reject({message: 'Lỗi server',error})
+        reject({ message: 'Lỗi server', error });
     }
-})
+});
 
 export default {getAllCompany}
