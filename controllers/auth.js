21cff7a41bd5d3ffcbadb6a428db75865a181946
doanvanquoc <vanquoc.dev@gmail.com>
2024-01-16 20:18:31 +0700

    fix bug register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
-import authService from '../services/auth.js'
+import authService from "../services/auth.js";
 const cloudinary = require("cloudinary").v2;
 
 const login = async (req, res) => {
@@ -9,25 +9,48 @@ const login = async (req, res) => {
     res.status(400).json({ message: "Vui lòng điền đầy đủ thông tin" });
   }
   try {
-    const result = await authService.login(req.body)
-    res.json(result)
+    const result = await authService.login(req.body);
+    res.json(result);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 };
 
 const register = async (req, res) => {
   const { email, password, fullname, birthday, phone_number, sex } = req.body;
-  const avatar = req.file.path
-  if (!email || !password || !fullname || !phone_number || !avatar || !birthday || !sex) {
-    cloudinary.uploader.destroy(req.file.filename)
+  let avatar;
+
+  if (!req.file) {
+    avatar =
+      "https://res.cloudinary.com/dxe8ykmrn/image/upload/v1705375410/user-avatar/tgaudfhwukm4c6gm0zzy.jpg";
+  } else {
+    avatar = req.file.path;
+  }
+
+  if (!email || !password || !fullname || !phone_number || !birthday || !sex) {
+    if (req.file) {
+      cloudinary.uploader.destroy(req.file.filename);
+    }
     return res.status(400).json({ message: "Vui lòng điền đầy đủ thông tin" });
   }
+
   try {
-    const result = await authService.register(req.body, req.file)
-    res.json(result)
+    const result = await authService.register({
+      email,
+      password,
+      fullname,
+      birthday,
+      phone_number,
+      sex,
+      file: req.file,
+    });
+
+    res.json(result);
   } catch (error) {
-    res.status(500).json(error)
+    if (req.file) {
+      cloudinary.uploader.destroy(req.file.filename);
+    }
+    res.status(500).json(error);
   }
 };
 
--- services/auth.js
+++ services/auth.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 const cloudinary = require("cloudinary").v2;
 
 const login = ({ email, password }) =>
@@ -14,8 +14,8 @@ const login = ({ email, password }) =>
         },
       });
       if (user) {
-        console.log(process.env.SECRET_KEY)
-        const token = jwt.sign({user}, process.env.SECRET_KEY, {
+        console.log(process.env.SECRET_KEY);
+        const token = jwt.sign({ user }, process.env.SECRET_KEY, {
           expiresIn: "1d",
         });
         resolve({ message: "OK", token });
@@ -23,28 +23,29 @@ const login = ({ email, password }) =>
         resolve({ message: "Tài khoản hoặc mật khẩu không chính xác" });
       }
     } catch (error) {
-      
       reject({ message: `Lỗi server: ${error}` });
     }
   });
-
-const register = ({
-  email,
-  password,
-  fullname,
-  birthday,
-  phone_number,
-  sex
-}, file) =>
+const register = (userData) =>
   new Promise(async (resolve, reject) => {
     try {
+      const { email, password, fullname, birthday, phone_number, sex, file } =
+        userData;
+
       const check = await db.User.findOne({
         where: { email },
       });
+
       if (check) {
-        cloudinary.uploader.destroy(file.filename)
+        if (file) {
+          cloudinary.uploader.destroy(file.filename);
+        }
         resolve({ message: "Email đã tồn tại" });
       } else {
+        const avatarPath = file
+          ? file.path
+          : "https://res.cloudinary.com/dxe8ykmrn/image/upload/v1705375410/user-avatar/tgaudfhwukm4c6gm0zzy.jpg";
+
         const user = await db.User.create(
           {
             email,
@@ -52,23 +53,34 @@ const register = ({
             fullname,
             birthday,
             phone_number,
-            avatar: file.path,
-            sex
-          }, {raw: true}
+            avatar: avatarPath,
+            sex,
+          },
+          { raw: true }
         );
+
         if (user) {
-          const token = jwt.sign({email: user.email, fullname: user.fullname, birthday: user.birthday, phone_number: user.phone_number, avatar: user.avatar, sex: user.sex}, process.env.SECRET_KEY, {
-            expiresIn: "1d",
-          });
-          console.log(jwt.verify(token, process.env.SECRET_KEY))
+          const token = jwt.sign(
+            {email: user.email, fullname: user.fullname, birthday: user.birthday, phone_number: user.phone_number, avatar: user.avatar, sex: user.sex},
+            process.env.SECRET_KEY,
+            {
+              expiresIn: "1d",
+            }
+          );
+
+          console.log(jwt.verify(token, process.env.SECRET_KEY));
           resolve({ message: "OK", token });
         } else {
-          cloudinary.uploader.destroy(file.filename)
+          if (file) {
+            cloudinary.uploader.destroy(file.filename);
+          }
           resolve({ message: "Không thể tạo mới người dùng" });
         }
       }
     } catch (error) {
-      cloudinary.uploader.destroy(file.filename)
+      if (userData.file) {
+        cloudinary.uploader.destroy(userData.file.filename);
+      }
       reject({ message: `Lỗi server: ${error}` });
     }
   });
