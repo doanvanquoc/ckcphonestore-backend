afe8565495e90e7d317389d9e0c5604e4d87b5ab
doanvanquoc <vanquoc.dev@gmail.com>
2024-01-17 08:49:21 +0700

    chỉnh method delete

--- routers/cart.js
+++ routers/cart.js
@@ -4,6 +4,6 @@ import cartController from '../controllers/cart.js'
 const router = express.Router()
 
 router.get('/:userID', cartController.getAllProduct)
-router.get('/delete/:productID', cartController.deleteProduct)
+router.delete('/:productID', cartController.deleteProduct)
 
 export default router
