4de6de79e1e5c650cb146d9ca2f16dc915328dfd
doanvanquoc <vanquoc.dev@gmail.com>
2024-02-23 10:16:15 +0700

    fix order:

--- controllers/order.js
+++ controllers/order.js
@@ -1,14 +1,14 @@
 import orderService from "../services/order.js";
 
 const createOrder = async (req, res) => {
-  const id = req.body.id;
-  if (!id) {
+  const { id, promotion } = req.body;
+  if (!id || !promotion) {
     return res
       .status(400)
       .json({ code: 0, message: "Vui lòng điền đầy đủ thông tin" });
   }
   try {
-    const result = await orderService.createOrder(id);
+    const result = await orderService.createOrder(id, promotion);
     res.json(result);
   } catch (error) {
     res.status(500).json(error);
